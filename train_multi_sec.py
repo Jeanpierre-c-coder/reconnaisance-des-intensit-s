114852b0dd1d0651c3f01d5781d3675f605d4e14
Jeanpierre-c-coder <kondejean3@gmail.com>
2025-03-22 18:20:40 +0100

    Update train_multi_sec.py

--- train_multi_sec.py
+++ train_multi_sec.py
@@ -2,7 +2,7 @@
 """
 Offline training/testing with elementary expressions from combined datasets of
 several participants
-By Sébastien Mick
+Rewrite By Jean Pierre KONDE MONK
 """
 # # IMPORTS
 # - Built-in
